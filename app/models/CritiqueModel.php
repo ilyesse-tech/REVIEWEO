e53e858b6afdab506961ec4409cb4316d5dd074c
ilyesse-tech <ilyesse.lazar@gmail.com>
2026-04-02 01:57:19 +0200

    critique model add critique , add like get critique get like fait

--- app/models/CritiqueModel.php
+++ app/models/CritiqueModel.php
@@ -2,20 +2,21 @@
 
 namespace app\models;
 
-class CritiqueModel{
+class CritiqueModel
+{
     //on donne accès a la base de donnée 
     private $connection;
 
     public function __construct($db_connection){
-        $this-> connection = $db_connection;
+        $this->connection = $db_connection;
     }
 
     //recu depuis le controller
-    public function addCritique(string $titre, string $contenu,int $note ,int $id_user, int $id_livre):bool{
-        $requestSQL= "INSERT INTO critique(titre, contenu, note, id_user, id_livre)
+    public function addCritique(string $titre, string $contenu, int $note, int $id_user, int $id_livre): bool{
+        $requestSQL = "INSERT INTO critique(titre, contenu, note, id_user, id_livre)
         VALUES (:titre, :contenu, :note, :id_user, :id_livre )
         ";
-        $statement=$this->connection->prepare($requestSQL);
+        $statement = $this->connection->prepare($requestSQL);
         return $statement->execute([  //la fonction register retournera donc true ou false au controller  
             ':titre' => $titre,
             ':contenu' => $contenu,
@@ -25,8 +26,46 @@ class CritiqueModel{
         ]);
     }
 
-    public function getCritiqueByLivre  (int $id_livre): array
+    public function addLike(int $id_user, int $id_livre): bool{
+        $requestSQL = "INSERT INTO like_critique(int $id_user, int $id_livre)
+        VALUES (:id_user, :id_livre )
+        ";
+        $statement = $this->connection->prepare($requestSQL);
+        return $statement->execute([  //la fonction register retournera donc true ou false au controller  
+            ':id_user' => $id_user,
+            ':id_livre' => $id_livre,
+        ]);
+    }
+
+    public function getCritiquesByLivre(int $id_livre): array{
+
+        $sql = "SELECT * FROM critique WHERE id_livre = :id";
+        
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([
+            ':id' => $id_livre
+        ]);
+
+        return $stmt->fetchAll(); // tableau de toute les critiques du livre avec l'id  qu'on va recup depuis une url avec un get
+    }
+
+
+    //
+    public function getLikes(int $critique_id): int
+    {
+
+        $sql = "SELECT COUNT(*)
+            FROM like_critique 
+            WHERE critique_id = :id";
+
+        $stmt = $this->connection->prepare($sql);
+
+        $stmt->execute([
+            ':id' => $critique_id
+        ]);
+
+        $result = $stmt->fetch(); // obliger meme si la requete sql est censer renvoyer juste un nombre execute ne renvoie que true ou false
+
+        return $result[0];
+    }
 }
-
-
-?>
